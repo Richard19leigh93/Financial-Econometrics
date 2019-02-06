363f591d7bc61775c68c0cb2238c4a27870c83f1
Richard Alistair Leigh <42261219+Richard19leigh93@users.noreply.github.com>
2019-02-06 22:09:08 +0200

    Delete H1_VaR_1W_sim_func.R

--- Final Project/code/H1_VaR_1W_sim_func.R
+++
@@ -1,7 +0,0 @@
-# VaR for simulations (1 Week from 2018-10-16 to 2018-10-22)
-H1_VaR_1W_sim_func <- function(x){x %>% xts_tbl() %>% 
-  gather(Sim, Return, -date) %>% 
-  tbl_xts() %>% 
-  VaR(p = 0.95, method = "modified")
-  
-  }
